09c400b1b58f3c8efb8e0bd07975d02559fdddca
Alessandro Loffredo <alessandro.loffredo@immobiliare.it>
2017-08-22 11:47:13 +0200

    edit alias git log: gl

--- modules/git/alias.zsh
+++ modules/git/alias.zsh
@@ -100,7 +100,7 @@ alias gix='git rm -r --cached'
 alias giX='git rm -rf --cached'
 
 # Log (l)
-alias gl='git log --topo-order --pretty=format:"${_git_log_medium_format}"'
+alias gl='git log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit'
 alias gls='git log --topo-order --stat --pretty=format:"${_git_log_medium_format}"'
 alias gld='git log --topo-order --stat --patch --full-diff --pretty=format:"${_git_log_medium_format}"'
 alias glo='git log --topo-order --pretty=format:"${_git_log_oneline_format}"'
